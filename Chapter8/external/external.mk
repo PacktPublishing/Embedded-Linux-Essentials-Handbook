dbcb3daba3c0745be83e607474f51d9a8f3b402e
Mohammed Billoo <mab@mab-labs.com>
2024-04-08 16:10:49 -0400

    Update directory structure

---
+++ Chapter8/external/external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_ELEHBR_PATH)/package/*/*.mk))
--- Chapter8/external/package/elehbr/elehbr.mk
+++ Chapter8/external/package/elehbr/elehbr.mk
@@ -1,19 +1,21 @@
 ################################################################################
 #
-# eleh-br package
+# elehbr package
 #
 ################################################################################
 
 ELEHBR_VERSION = 1.0
-ELEHBR_SITE = package/elehbr/src
+ELEHBR_SITE = $(BR2_EXTERNAL_ELEHBR_PATH)/package/elehbr/src
 ELEHBR_SITE_METHOD = local
 
 define ELEHBR_BUILD_CMDS
     $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
 endef
 
+ELEHBR_INSTALL_TARGET = YES
+
 define ELEHBR_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/eleh-br  $(TARGET_DIR)/usr/bin
+    $(INSTALL) -D -m 0755 $(@D)/elehbr  $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
